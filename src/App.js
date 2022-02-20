e82f46635b33015c8890d08a771fae6a7aa1b0c1
Umang A <umanga.eng@gmail.com>
2022-02-20 15:50:29 -0500

    created links in the navbar component, button component created along with its css.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import "./App.css";
-
+// import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact />
+        </Routes>
+      </Router>
       <h1>Project setup</h1>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,53 @@
 import React, { useState } from "react";
-
+import { Link } from "react-router-dom";
 export default function Navbar() {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick((click) => !click);
+  const closeMobileMenu = () => setClick(false);
   return (
     <div>
       <nav className="navbar">
         <div className="navbar-container">
           <Link to="/" className="navbar-logo">
-            Travellers <i className="fab fa-typo"></i>
+            Travellers <i className="fab fa-typo3"></i>
           </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/services"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Services
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/products"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Products
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/sign-up"
+                className="nav-links-mobile"
+                onClick={closeMobileMenu}
+              >
+                Sign Up
+              </Link>
+            </li>
+          </ul>
         </div>
       </nav>
     </div>
